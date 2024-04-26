c63dd70803ff9772d5247b64f6b60c1e66515717
Miguel Martinez <mmartinez@startnet.co>
2024-04-25 21:35:10 -0500

    CAMBIOS PAGINA

--- assets/html/header.php
+++ assets/html/header.php
@@ -2,7 +2,8 @@
 <section id="topbar" class="topbar d-flex align-items-center">
     <div class="container d-flex justify-content-center justify-content-md-between">
         <div class="contact-info d-flex align-items-center">
-            <i class="bi bi-envelope d-flex align-items-center"><a href="mailto:contact@example.com">contact@example.com</a></i>
+            <i class="bi bi-envelope d-flex align-items-center"><a
+                    href="mailto:contact@example.com">contact@example.com</a></i>
             <i class="bi bi-phone d-flex align-items-center ms-4"><span>+1 5589 55488 55</span></i>
         </div>
         <div class="social-links d-none d-md-flex align-items-center">
@@ -17,20 +18,19 @@
 <header id="header" class="header d-flex align-items-center">
 
     <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
-        <a href="index.html" class="logo d-flex align-items-center">
+        <a href="index" class="logo d-flex align-items-center">
             <!-- Uncomment the line below if you also wish to use an image logo -->
             <!-- <img src="assets/img/logo.png" alt=""> -->
             <img src="/assets/image/logo_principal.png" alt="Logo" style="max-width: 300px;">
         </a>
-        <nav id="navbar" class="navbar">
+        <nav id="navbar" class="navbar ">
             <ul>
-                <li><a href="index">Inicio</a></li>
-                <li><a href="#about">Beneficios</a></li>
-                <li><a href="#services">Comunidad</a></li>
-                <li><a href="#portfolio">Puntos</a></li>
-                <li><a href="#portfolio">Unete</a></li>
-                <li><a href="blog">Blog</a></li>
-                <li><a href="">Ingresar</a></li>
+                <li><a class="text-white" href="index"><i class="bi bi-house-door-fill pe-1"  width="32" height="32"></i> Inicio</a></li>
+                <li><a class="text-white" href="#about"><i class="bi bi-gift pe-1"></i> Beneficios</a></li>
+                <li><a class="text-white" href="#services"><i class="bi bi-people-fill pe-1"></i> Comunidad</a></li>
+                <li><a class="text-white" href="#portfolio"><i class="bi bi-coin pe-1"></i> Puntos</a></li>
+                <li><a class="text-white" href="#portfolio"><i class="bi bi-person-fill-add pe-1"></i> Unete</a></li>
+                <li><a class="text-white" href=""><i class="bi bi-person-fill-lock pe-1"></i> Ingresar</a></li>
             </ul>
         </nav><!-- .navbar -->
     </div>
