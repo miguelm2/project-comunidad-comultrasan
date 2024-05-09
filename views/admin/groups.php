f0f46612bed8b22d214ce15ebcd3c10c36307f4f
Santiago-Kondory <163871166+Santiago-Kondory@users.noreply.github.com>
2024-05-09 11:12:52 -0500

    Cambio de paleta de colores
    Se hace el cambio de la paleta de colores, en la cual se elimina la parte innecesaria de codigo que habia en proyecto

--- views/admin/groups.php
+++ views/admin/groups.php
@@ -89,7 +89,7 @@
                         <td>
                            <div class="d-flex px-2 py-1">
                               <div>
-                                 <img src="assets/img/small-logos/logo-xd.svg" class="avatar avatar-sm me-3" alt="xd">
+                                 <img src="../assets/img/small-logos/logo-xd.svg" class="avatar avatar-sm me-3" alt="xd">
                               </div>
                               <div class="d-flex flex-column justify-content-center">
                                  <h6 class="mb-0 text-sm">Grupo 1</h6>
@@ -99,16 +99,16 @@
                         <td>
                            <div class="avatar-group mt-2">
                               <a href="javascript:;" class="avatar avatar-xs rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Ryan Tompson">
-                                 <img src="assets/img/team-1.jpg" alt="team1">
+                                 <img src="../assets/img/team-1.jpg" alt="team1">
                               </a>
                               <a href="javascript:;" class="avatar avatar-xs rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Romina Hadid">
-                                 <img src="assets/img/team-2.jpg" alt="team2">
+                                 <img src="../assets/img/team-2.jpg" alt="team2">
                               </a>
                               <a href="javascript:;" class="avatar avatar-xs rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Alexander Smith">
-                                 <img src="assets/img/team-3.jpg" alt="team3">
+                                 <img src="../assets/img/team-3.jpg" alt="team3">
                               </a>
                               <a href="javascript:;" class="avatar avatar-xs rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Jessica Doe">
-                                 <img src="assets/img/team-4.jpg" alt="team4">
+                                 <img src="../assets/img/team-4.jpg" alt="team4">
                               </a>
                            </div>
                         </td>
@@ -131,7 +131,7 @@
                         <td>
                            <div class="d-flex px-2 py-1">
                               <div>
-                                 <img src="assets/img/small-logos/logo-atlassian.svg" class="avatar avatar-sm me-3" alt="atlassian">
+                                 <img src="../assets/img/small-logos/logo-atlassian.svg" class="avatar avatar-sm me-3" alt="atlassian">
                               </div>
                               <div class="d-flex flex-column justify-content-center">
                                  <h6 class="mb-0 text-sm">Grupo 2</h6>
@@ -141,10 +141,10 @@
                         <td>
                            <div class="avatar-group mt-2">
                               <a href="javascript:;" class="avatar avatar-xs rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Romina Hadid">
-                                 <img src="assets/img/team-2.jpg" alt="team5">
+                                 <img src="../assets/img/team-2.jpg" alt="team5">
                               </a>
                               <a href="javascript:;" class="avatar avatar-xs rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Jessica Doe">
-                                 <img src="assets/img/team-4.jpg" alt="team6">
+                                 <img src="../assets/img/team-4.jpg" alt="team6">
                               </a>
                            </div>
                         </td>
@@ -165,7 +165,7 @@
                         <td>
                            <div class="d-flex px-2 py-1">
                               <div>
-                                 <img src="assets/img/small-logos/logo-slack.svg" class="avatar avatar-sm me-3" alt="team7">
+                                 <img src="../assets/img/small-logos/logo-slack.svg" class="avatar avatar-sm me-3" alt="team7">
                               </div>
                               <div class="d-flex flex-column justify-content-center">
                                  <h6 class="mb-0 text-sm">Grupo 3</h6>
@@ -175,10 +175,10 @@
                         <td>
                            <div class="avatar-group mt-2">
                               <a href="javascript:;" class="avatar avatar-xs rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Romina Hadid">
-                                 <img src="assets/img/team-3.jpg" alt="team8">
+                                 <img src="../assets/img/team-3.jpg" alt="team8">
                               </a>
                               <a href="javascript:;" class="avatar avatar-xs rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Jessica Doe">
-                                 <img src="assets/img/team-1.jpg" alt="team9">
+                                 <img src="../assets/img/team-1.jpg" alt="team9">
                               </a>
                            </div>
                         </td>
@@ -199,67 +199,6 @@
          </div>
       </div>
    </main>
-   <div class="fixed-plugin">
-      <a class="fixed-plugin-button text-dark position-fixed px-3 py-2">
-         <i class="material-icons py-2">settings</i>
-      </a>
-      <div class="card shadow-lg">
-         <div class="card-header pb-0 pt-3">
-            <div class="float-start">
-               <h5 class="mt-3 mb-0">Material UI Configurator</h5>
-               <p>See our dashboard options.</p>
-            </div>
-            <div class="float-end mt-4">
-               <button class="btn btn-link text-dark p-0 fixed-plugin-close-button">
-                  <i class="material-icons">clear</i>
-               </button>
-            </div>
-            <!-- End Toggle Button -->
-         </div>
-         <hr class="horizontal dark my-1">
-         <div class="card-body pt-sm-3 pt-0">
-            <!-- Sidebar Backgrounds -->
-            <div>
-               <h6 class="mb-0">Sidebar Colors</h6>
-            </div>
-            <a href="javascript:void(0)" class="switch-trigger background-color">
-               <div class="badge-colors my-2 text-start">
-                  <span class="badge filter bg-gradient-primary active" data-color="primary" onclick="sidebarColor(this)"></span>
-                  <span class="badge filter bg-gradient-dark" data-color="dark" onclick="sidebarColor(this)"></span>
-                  <span class="badge filter bg-gradient-info" data-color="info" onclick="sidebarColor(this)"></span>
-                  <span class="badge filter bg-gradient-success" data-color="success" onclick="sidebarColor(this)"></span>
-                  <span class="badge filter bg-gradient-warning" data-color="warning" onclick="sidebarColor(this)"></span>
-                  <span class="badge filter bg-gradient-danger" data-color="danger" onclick="sidebarColor(this)"></span>
-               </div>
-            </a>
-            <!-- Sidenav Type -->
-            <div class="mt-3">
-               <h6 class="mb-0">Sidenav Type</h6>
-               <p class="text-sm">Choose between 2 different sidenav types.</p>
-            </div>
-            <div class="d-flex">
-               <button class="btn bg-gradient-dark px-3 mb-2 active" data-class="bg-gradient-dark" onclick="sidebarType(this)">Dark</button>
-               <button class="btn bg-gradient-dark px-3 mb-2 ms-2" data-class="bg-transparent" onclick="sidebarType(this)">Transparent</button>
-               <button class="btn bg-gradient-dark px-3 mb-2 ms-2" data-class="bg-white" onclick="sidebarType(this)">White</button>
-            </div>
-            <p class="text-sm d-xl-none d-block mt-2">You can change the sidenav type just on desktop view.</p>
-            <!-- Navbar Fixed -->
-            <div class="mt-3 d-flex">
-               <h6 class="mb-0">Navbar Fixed</h6>
-               <div class="form-check form-switch ps-0 ms-auto my-auto">
-                  <input class="form-check-input mt-1 ms-auto" type="checkbox" id="navbarFixed" onclick="navbarFixed(this)">
-               </div>
-            </div>
-            <hr class="horizontal dark my-3">
-            <div class="mt-2 d-flex">
-               <h6 class="mb-0">Light / Dark</h6>
-               <div class="form-check form-switch ps-0 ms-auto my-auto">
-                  <input class="form-check-input mt-1 ms-auto" type="checkbox" id="dark-version" onclick="darkMode(this)">
-               </div>
-            </div>
-         </div>
-      </div>
-   </div>
    <!--   Core JS Files   -->
    <script src="../assets/js/core/popper.min.js"></script>
    <script src="../assets/js/core/bootstrap.min.js"></script>
--- views/admin/index.php
+++ views/admin/index.php
@@ -60,7 +60,7 @@
             <div class="col-xl-4 col-sm-6 mb-xl-0 mb-4">
                <div class="card">
                   <div class="card-header p-3 pt-2">
-                     <div class="icon icon-lg icon-shape bg-gradient-primary shadow-primary text-center border-radius-xl mt-n4 position-absolute">
+                     <div class="icon icon-lg icon-shape bg-gradient-info shadow-primary text-center border-radius-xl mt-n4 position-absolute">
                         <i class="material-icons opacity-10">people</i>
                      </div>
                      <div class="text-end pt-1">
@@ -88,7 +88,7 @@
             <div class="col-lg-4 col-md-6 mt-4 mb-4">
                <div class="card z-index-2 ">
                   <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2 bg-transparent">
-                     <div class="bg-gradient-primary shadow-primary border-radius-lg py-3 pe-1">
+                     <div class="bg-gradient-info shadow-primary border-radius-lg py-3 pe-1">
                         <div class="chart">
                            <canvas id="chart-bars" class="chart-canvas" height="170"></canvas>
                         </div>
@@ -271,67 +271,6 @@
             </div>
          </div>
    </main>
-   <div class="fixed-plugin">
-      <a class="fixed-plugin-button text-dark position-fixed px-3 py-2">
-         <i class="material-icons py-2">settings</i>
-      </a>
-      <div class="card shadow-lg">
-         <div class="card-header pb-0 pt-3">
-            <div class="float-start">
-               <h5 class="mt-3 mb-0">Material UI Configurator</h5>
-               <p>See our dashboard options.</p>
-            </div>
-            <div class="float-end mt-4">
-               <button class="btn btn-link text-dark p-0 fixed-plugin-close-button">
-                  <i class="material-icons">clear</i>
-               </button>
-            </div>
-            <!-- End Toggle Button -->
-         </div>
-         <hr class="horizontal dark my-1">
-         <div class="card-body pt-sm-3 pt-0">
-            <!-- Sidebar Backgrounds -->
-            <div>
-               <h6 class="mb-0">Sidebar Colors</h6>
-            </div>
-            <a href="javascript:void(0)" class="switch-trigger background-color">
-               <div class="badge-colors my-2 text-start">
-                  <span class="badge filter bg-gradient-primary active" data-color="primary" onclick="sidebarColor(this)"></span>
-                  <span class="badge filter bg-gradient-dark" data-color="dark" onclick="sidebarColor(this)"></span>
-                  <span class="badge filter bg-gradient-info" data-color="info" onclick="sidebarColor(this)"></span>
-                  <span class="badge filter bg-gradient-success" data-color="success" onclick="sidebarColor(this)"></span>
-                  <span class="badge filter bg-gradient-warning" data-color="warning" onclick="sidebarColor(this)"></span>
-                  <span class="badge filter bg-gradient-danger" data-color="danger" onclick="sidebarColor(this)"></span>
-               </div>
-            </a>
-            <!-- Sidenav Type -->
-            <div class="mt-3">
-               <h6 class="mb-0">Sidenav Type</h6>
-               <p class="text-sm">Choose between 2 different sidenav types.</p>
-            </div>
-            <div class="d-flex">
-               <button class="btn bg-gradient-dark px-3 mb-2 active" data-class="bg-gradient-dark" onclick="sidebarType(this)">Dark</button>
-               <button class="btn bg-gradient-dark px-3 mb-2 ms-2" data-class="bg-transparent" onclick="sidebarType(this)">Transparent</button>
-               <button class="btn bg-gradient-dark px-3 mb-2 ms-2" data-class="bg-white" onclick="sidebarType(this)">White</button>
-            </div>
-            <p class="text-sm d-xl-none d-block mt-2">You can change the sidenav type just on desktop view.</p>
-            <!-- Navbar Fixed -->
-            <div class="mt-3 d-flex">
-               <h6 class="mb-0">Navbar Fixed</h6>
-               <div class="form-check form-switch ps-0 ms-auto my-auto">
-                  <input class="form-check-input mt-1 ms-auto" type="checkbox" id="navbarFixed" onclick="navbarFixed(this)">
-               </div>
-            </div>
-            <hr class="horizontal dark my-3">
-            <div class="mt-2 d-flex">
-               <h6 class="mb-0">Light / Dark</h6>
-               <div class="form-check form-switch ps-0 ms-auto my-auto">
-                  <input class="form-check-input mt-1 ms-auto" type="checkbox" id="dark-version" onclick="darkMode(this)">
-               </div>
-            </div>
-         </div>
-      </div>
-   </div>
    <!--   Core JS Files   -->
    <script src="../assets/js/core/popper.min.js"></script>
    <script src="../assets/js/core/bootstrap.min.js"></script>
