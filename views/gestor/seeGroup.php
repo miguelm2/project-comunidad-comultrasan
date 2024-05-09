734033e3b4f2b57970671f4d3f1d5bdcfac1b5d2
Santiago-Kondory <163871166+Santiago-Kondory@users.noreply.github.com>
2024-05-09 16:49:40 -0500

    Arreglo user y breadcum
    Se hace arreglo de la vista del user que queden mejor las cards y se coloca una tabla con los beneficos que tiene el usuario, y se elimina las breadcum de todas las vistas del usuario

--- views/gestor/seeGroup.php
+++ views/gestor/seeGroup.php
@@ -29,13 +29,6 @@
       <!-- Navbar -->
       <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur" data-scroll="true">
          <div class="container-fluid py-1 px-3">
-            <nav aria-label="breadcrumb">
-               <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
-                  <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">Pages</a></li>
-                  <li class="breadcrumb-item text-sm"><a class="opcaity-5 text-dark" href="comunity">Grupos</a></li>
-                  <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Ver Grupo</li>
-               </ol>
-            </nav>
             <!-- Start header -->
             <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/views/assets/html/header_admin.php'; ?>
             <!-- End header -->
