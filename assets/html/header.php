aef286faf6422c04b3f3c349321cfced0963618f
Santiago-Kondory <163871166+Santiago-Kondory@users.noreply.github.com>
2024-06-21 17:02:36 -0500

    Cambios Solicitados
    Se hacen los cambios solicitados de acuerdo a las imagenes, pdf, y presentación enviados

--- assets/html/header.php
+++ assets/html/header.php
@@ -1,9 +1,7 @@
-<div class="header-top" style="display: block; unicode-bidi: isolate;">
-</div>
 <header id="header" class="header d-flex align-items-center bg-light">
     <div class="container-fluid container-xl d-flex align-items-center justify-content-between bg-light">
         <a href="index" class="logo d-flex align-items-center">
-            <img src="assets/image/logo_principal.png" alt="Logo_financiera">
+            <img src="assets/image/logo_principal.png" alt="Logo_financiera" class="img-fluid">
         </a>
         <nav id="navbar" class="navbar">
             <ul>
@@ -22,7 +20,7 @@
                         Beneficios y Recompensas
                     </a>
                 </li>
-                <a href="login#login" class="btn btn-outline-success text-primary btn1 p-2 m-3">Ingresar</a>
+                <a href="login#login" class="btn btn-outline-success text-primary btn1 p-1 m-3">Ingresar</a>
             </ul>
         </nav><!-- .navbar -->
         <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
--- index.php
+++ index.php
@@ -39,7 +39,7 @@
    <!-- ======= Hero Section ======= -->
    <section id="index" class="hero bg-light">
       <div class="container">
-         <img src="assets/image/slider_image.png" class="img-fluid" alt="" data-aos="zoom-out" data-aos-delay="100">
+         <img src="assets/image/slider_family.jpg" class="img-fluid rounded-4" alt="" data-aos="zoom-out" data-aos-delay="100">
          <div class="text-overlay">
             <h4>¡Bienvenido a la Comunidad Familia Financiera Comultrasan!</h4>
             <div class="row justify-content-center">
@@ -58,13 +58,13 @@
    <main id="main">
       <!-- ======= Stats Counter Section ======= -->
       <div>
-         <section id="comunidad" class="stats-counter">
+         <section id="comunidad">
             <div class="container" data-aos="fade-up">
                <div class="row gy-2 align-items-center">
-                  <div class="col-lg-4">
-                     <img src="assets/image/familia.png" alt="" class="img-fluid rounded-4">
+                  <div class="col-lg-3">
+                     <img src="assets/image/joven.png" alt="" class="img-fluid rounded-4">
                   </div>
-                  <div class="col-lg-8">
+                  <div class="col-lg-9">
                      <div class="text-center">
                         <h3>Comparte tus gustos e intereses con integrantes de otras comunidades</h3>
                      </div>
@@ -86,18 +86,113 @@
          </section>
       </div>
       <!-- End Stats Counter Section -->
-
+      <section class="bg-color-page">
+         <div class=" mt-2 m-3">
+            <h2 class="text-start text-white m-3">Tu historia es nuestro propósito</h2>
+            <p class="text-start text-white m-3">Testimonios reales de éxito y progreso de nuestros clientes</p>
+            <div id="testimoniosCarousel" class="carousel slide" data-bs-ride="carousel">
+               <div class="carousel-inner">
+                  <div class="carousel-item active">
+                     <div class="row">
+                        <div class="col-md-4">
+                           <div class="card flex-row">
+                              <img src="assets/image/imagen-1.jpg" class="card-img-bottom img-fluid p-2 rounded-4" alt="Testimonio 1">
+                              <div class="card-body">
+                                 <h5 class="card-title">Seguro de hogar</h5>
+                                 <p class="card-text">Gracias al seguro pude reconstruir mi hogar y nos recuperamos de un incendio.</p>
+                                 <p class="text-muted">Myriam Fonseca - Tendera</p>
+                                 <a href="#" class="btn btn-primary">¡Me interesa!</a>
+                              </div>
+                           </div>
+                        </div>
+                        <div class="col-md-4">
+                           <div class="card flex-row">
+                              <img src="assets/image/imagen-2.jpg" class="card-img-bottom img-fluid p-2 rounded-4" alt="Testimonio 2">
+                              <div class="card-body">
+                                 <h5 class="card-title">Préstamo en línea</h5>
+                                 <p class="card-text">He adquirido 2 préstamos, la fórmula es invertir siempre en un negocio que produzca dinero.</p>
+                                 <p class="text-muted">Miguel Bolívar - Barbero</p>
+                                 <a href="#" class="btn btn-success">¡Iniciar desembolso!</a>
+                              </div>
+                           </div>
+                        </div>
+                        <div class="col-md-4">
+                           <div class="card flex-row">
+                              <img src="assets/image/imagen-3.jpg" class="card-img-bottom img-fluid p-2 rounded-4" alt="Testimonio 3">
+                              <div class="card-body">
+                                 <h5 class="card-title">Crédito de negocio</h5>
+                                 <p class="card-text">Gracias a Crezcamos pude adquirir maquinaria nueva y hemos aumentado la producción mensual de zapatos.</p>
+                                 <p class="text-muted">Erika Patiño - Emprendedora</p>
+                                 <a href="#" class="btn btn-primary">¡Iniciar desembolso!</a>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="carousel-item">
+                     <div class="row">
+                        <div class="col-md-4">
+                           <div class="card flex-row">
+                              <img src="assets/image/imagen-1.jpg" class="card-img-bottom img-fluid p-2 rounded-4" alt="Testimonio 4">
+                              <div class="card-body">
+                                 <h5 class="card-title">Seguro de hogar</h5>
+                                 <p class="card-text">Gracias al seguro pude reconstruir mi hogar y nos recuperamos de un incendio.</p>
+                                 <p class="text-muted">Myriam Fonseca - Tendera</p>
+                                 <a href="#" class="btn btn-primary">¡Me interesa!</a>
+                              </div>
+                           </div>
+                        </div>
+                        <div class="col-md-4">
+                           <div class="card flex-row">
+                              <img src="assets/image/imagen-2.jpg" class="card-img-bottom img-fluid p-2 rounded-4" alt="Testimonio 5">
+                              <div class="card-body">
+                                 <h5 class="card-title">Préstamo en línea</h5>
+                                 <p class="card-text">He adquirido 2 préstamos, la fórmula es invertir siempre en un negocio que produzca dinero.</p>
+                                 <p class="text-muted">Miguel Bolívar - Barbero</p>
+                                 <a href="#" class="btn btn-success">¡Iniciar desembolso!</a>
+                              </div>
+                           </div>
+                        </div>
+                        <div class="col-md-4">
+                           <div class="card flex-row">
+                              <img src="assets/image/imagen-3.jpg" class="card-img-bottom img-fluid p-2 rounded-4" alt="Testimonio 6">
+                              <div class="card-body">
+                                 <h5 class="card-title">Crédito de negocio</h5>
+                                 <p class="card-text">Gracias a Crezcamos pude adquirir maquinaria nueva y hemos aumentado la producción mensual de zapatos.</p>
+                                 <p class="text-muted">Erika Patiño - Emprendedora</p>
+                                 <a href="#" class="btn btn-primary">¡Iniciar desembolso!</a>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <button class="carousel-control-prev" type="button" data-bs-target="#testimoniosCarousel" data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Previous</span>
+               </button>
+               <button class="carousel-control-next" type="button" data-bs-target="#testimoniosCarousel" data-bs-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Next</span>
+               </button>
+            </div>
+         </div>
+      </section>
       <!-- ======= Our Services Section ======= -->
       <section class="services sections-bg bg-light">
          <div class="container mt-2 p-2">
-            <h2 class="text-center">Novedades de nuestras comunidad</h2>
+            <div class="row">
+               <div class="section-header pb-0">
+                  <h2 class="text-black">Novedades de nuestra comunidad</h2>
+               </div>
+            </div>
             <div id="novedadesCarousel" class="carousel slide" data-bs-ride="carousel">
                <div class="carousel-inner">
                   <div class="carousel-item active">
                      <div class="row">
                         <div class="col-md-4">
                            <div class="card">
-                              <img src="assets/image/imagen1.jpg" class="card-img-top" alt="Celebración día de la familia">
+                              <img src="assets/image/imagen1.jpg" class="card-img-top img-fluid" alt="Celebración día de la familia">
                               <div class="card-body">
                                  <h5 class="card-title">Celebración día de la familia</h5>
                                  <p class="card-text">Te invitamos a ser parte de un programa único diseñado especialmente para ti, tu familia y tus seres queridos. Al registrarte, podrás disfrutar de sistemas de recompensas, descuentos exclusivos...</p>
@@ -106,7 +201,7 @@
                         </div>
                         <div class="col-md-4">
                            <div class="card">
-                              <img src="assets/image/imagen2.jpg" class="card-img-top" alt="Beneficios para tus mascotas">
+                              <img src="assets/image/imagen2.jpg" class="card-img-top  img-fluid" alt="Beneficios para tus mascotas">
                               <div class="card-body">
                                  <h5 class="card-title">Beneficios para tus mascotas</h5>
                                  <p class="card-text">Te invitamos a ser parte de un programa único diseñado especialmente para ti, tu familia y tus seres queridos. Al registrarte, podrás disfrutar de sistemas de recompensas, descuentos exclusivos...</p>
@@ -115,7 +210,7 @@
                         </div>
                         <div class="col-md-4">
                            <div class="card">
-                              <img src="assets/image/imagen3.jpg" class="card-img-top" alt="Apoyos educativos">
+                              <img src="assets/image/imagen3.jpg" class="card-img-top img-fluid" alt="Apoyos educativos">
                               <div class="card-body">
                                  <h5 class="card-title">Apoyos educativos</h5>
                                  <p class="card-text">Te invitamos a ser parte de un programa único diseñado especialmente para ti, tu familia y tus seres queridos. Al registrarte, podrás disfrutar de sistemas de recompensas, descuentos exclusivos...</p>
@@ -128,7 +223,7 @@
                      <div class="row">
                         <div class="col-md-4">
                            <div class="card">
-                              <img src="assets/image/imagen1.jpg" class="card-img-top" alt="Celebración día de la familia">
+                              <img src="assets/image/imagen1.jpg" class="card-img-top img-fluid" alt="Celebración día de la familia">
                               <div class="card-body">
                                  <h5 class="card-title">Celebración día de la familia</h5>
                                  <p class="card-text">Te invitamos a ser parte de un programa único diseñado especialmente para ti, tu familia y tus seres queridos. Al registrarte, podrás disfrutar de sistemas de recompensas, descuentos exclusivos...</p>
@@ -167,6 +262,78 @@
             </div>
          </div>
       </section><!-- End Our Services Section -->
+      <!-- ======= Frequently Asked Questions Section ======= -->
+      <section class="faq sections-bg">
+         <div class="container" data-aos="fade-up">
+            <div class="row gy-4">
+               <div class="col-lg-4">
+                  <div class="content px-xl-5">
+                     <h3 class="text-black">Preguntas Frecuentes</h3>
+                     <p class="text-black">
+                        En Comultrasan, entendemos que cada paso en tu camino financiero puede venir acompañado de preguntas
+                        y dudas. Por eso, hemos reunido esta sección para ofrecerte respuestas claras y concisas a las consultas más comunes
+                        que nuestros clientes suelen tener.
+                     </p>
+                  </div>
+               </div>
+               <div class="col-lg-8">
+                  <div class="accordion accordion-flush" id="faqlist" data-aos="fade-up" data-aos-delay="100">
+                     <div class="accordion-item">
+                        <h3 class="accordion-header">
+                           <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-1">
+                              <span class="num">1.</span>
+                              ¿Cómo sumar puntos?
+                           </button>
+                        </h3>
+                        <div id="faq-content-1" class="accordion-collapse collapse" data-bs-parent="#faqlist">
+                           <div class="accordion-body">
+                              Feugiat pretium nibh ipsum consequat. Tempus iaculis urna id volutpat lacus laoreet non
+                              curabitur gravida. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus
+                              non.
+                           </div>
+                        </div>
+                     </div><!-- # Faq item-->
+
+                     <div class="accordion-item">
+                        <h3 class="accordion-header">
+                           <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-2">
+                              <span class="num">2.</span>
+                              ¿Cómo referir un amigo?
+                           </button>
+                        </h3>
+                        <div id="faq-content-2" class="accordion-collapse collapse" data-bs-parent="#faqlist">
+                           <div class="accordion-body">
+                              Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit
+                              laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec
+                              pretium. Est pellentesque elit ullamcorper dignissim. Mauris ultrices eros in cursus
+                              turpis massa tincidunt dui.
+                           </div>
+                        </div>
+                     </div><!-- # Faq item-->
+
+                     <div class="accordion-item">
+                        <h3 class="accordion-header">
+                           <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-3">
+                              <span class="num">3.</span>
+                              ¿Cómo unirme?
+                           </button>
+                        </h3>
+                        <div id="faq-content-3" class="accordion-collapse collapse" data-bs-parent="#faqlist">
+                           <div class="accordion-body">
+                              Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Faucibus
+                              pulvinar elementum integer enim. Sem nulla pharetra diam sit amet nisl suscipit. Rutrum
+                              tellus pellentesque eu tincidunt. Lectus urna duis convallis convallis tellus. Urna
+                              molestie at elementum eu facilisis sed odio morbi quis
+                           </div>
+                        </div>
+                     </div><!-- # Faq item-->
+
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section><!-- End Frequently Asked Questions Section -->
+
       <section>
          <div class="row justify-content-center">
             <div class="col-9">
