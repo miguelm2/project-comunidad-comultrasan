2b016d88184ad904f7351a6d6f5fac82fe091d1d
Santiago-Kondory <163871166+Santiago-Kondory@users.noreply.github.com>
2024-07-05 14:22:18 -0500

    Referidos
    Se hace la vista de referidos

---
+++ refer.php
@@ -0,0 +1,140 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Referidos</title>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="assets/image/favicon_0.ico" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@200..800&display=swap" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="assets/css/main.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <!-- Start Header -->
+    <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/assets/html/header.php'; ?>
+    <!-- End Header -->
+    <section class="ftco-section p-3" id="unete">
+        <div class="container">
+            <div class="text-center section-header pb-1">
+                <h2 class="text-black">Referidos</h2>
+            </div>
+            <div class="row">
+                <div class="col-md-12 col-lg-12">
+                    <div class="login-wrap">
+                        <form action="#" class="signin-form">
+                            <div class="row text-start">
+                                <div class="col-md-12">
+                                    <h5 class="text-black">
+                                        Datos de quien refiere
+                                    </h5>
+                                </div>
+                                <div class="col-md-6 mt-2">
+                                    <div class="form-group">
+                                        <label class="label text-black" for="name">Nombre y Apellidos</label>
+                                        <input type="text" class="form-control" placeholder="Nombre" required>
+                                    </div>
+                                </div>
+                                <div class="col-md-6 mt-2">
+                                    <div class="form-group">
+                                        <label class="label text-black" for="name">Nro de documento</label>
+                                        <input type="text" class="form-control" placeholder="Nombre" required>
+                                    </div>
+                                </div>
+                                <div class="col-md-12">
+                                    <h5 class="text-black">
+                                        Datos Referidos
+                                    </h5>
+                                </div>
+
+                                <div class="col-md-6 mt-2">
+                                    <div class="form-group">
+                                        <label class="label text-black" for="name">Nombre y Apellidos</label>
+                                        <input type="text" class="form-control" placeholder="Nombre" required>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-6 mt-2">
+                                    <div class="form-group">
+                                        <label class="label text-black" for="name">Nro de documento</label>
+                                        <input type="text" class="form-control" placeholder="Nombre" required>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-6 mt-2">
+                                    <div class="form-group">
+                                        <label class="label text-black" for="name">Departamento</label>
+                                        <input type="text" class="form-control" placeholder="Nombre" required>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-6 mt-2">
+                                    <div class="form-group">
+                                        <label class="label text-black" for="name">Ciudad</label>
+                                        <input type="text" class="form-control" placeholder="Nombre" required>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-6 mt-2">
+                                    <div class="form-group">
+                                        <label class="label text-black" for="mail">Correo</label>
+                                        <input type="email" class="form-control" placeholder="Correo" required>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-6 mt-2">
+                                    <div class="form-group">
+                                        <label class="label text-black" for="cellphone">Celular</label>
+                                        <input type="number" class="form-control" placeholder="Celular" required>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div class="form-group">
+                                <br>
+                                <button type="submit" class="form-control btn btn-success rounded submit px-3"> <i class="bi bi-send"></i> Enviar</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Start Footer -->
+    <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/assets/html/footer.php'; ?>
+    <!-- End Footer -->
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
