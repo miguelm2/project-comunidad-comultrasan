ba4a26b3b3ec13fa42384bdf27d0107123f04d08
Miguel Martinez <mmartinez@startnet.co>
2024-10-24 14:32:53 -0500

    Update benefitsPublicPage.php

--- system/views/user/benefitsPublicPage.php
+++ system/views/user/benefitsPublicPage.php
@@ -1,109 +1,67 @@
 <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/system/php/routing/User.php'; ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-   <meta charset="utf-8">
-   <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
-   <title>Comunidad Comultrasan - Inicio</title>
-   <meta content="" name="description">
-   <meta content="" name="keywords">
-
-   <!-- Favicons -->
-   <link href="assets/image/favicon_0.ico" rel="icon">
-   <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
-
-   <!-- Google Fonts -->
-   <link rel="preconnect" href="https://fonts.googleapis.com">
-   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+   <meta charset="utf-8" />
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+   <link rel="apple-touch-icon" sizes="76x76" href="/system/assets/img/apple-icon.png">
+   <link rel="icon" type="image/png" href="/system/assets/image/favicon_0.ico">
+   <title>Comunidad Comultrasan</title>
+   <!--     Fonts and icons     -->
+   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
    <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@200..800&display=swap" rel="stylesheet">
-
-   <!-- Vendor CSS Files -->
-   <link href="../../../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-   <link href="../../../assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-   <link href="../../../assets/vendor/aos/aos.css" rel="stylesheet">
-   <link href="../../../assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-   <link href="../../../assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
-   <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
-
-   <!-- Template Main CSS File -->
-   <link href="../../../assets/css/main.css" rel="stylesheet">
-   <link href="../../../assets/css/style.css" rel="stylesheet">
+   <!-- Nucleo Icons -->
+   <link href="/system/assets/css/nucleo-icons.css" rel="stylesheet" />
+   <link href="/system/assets/css/nucleo-svg.css" rel="stylesheet" />
+   <!-- Font Awesome Icons -->
+   <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+   <!-- Material Icons -->
+   <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
+   <!-- CSS Files -->
+   <link id="pagestyle" href="/system/assets/css/material-dashboard.css?v=3.1.0" rel="stylesheet" />
+   <script defer data-site="YOUR_DOMAIN_HERE" src="https://api.nepcha.com/js/nepcha-analytics.js"></script>
 </head>
 
-<body class="d-flex flex-column min-vh-100">
-
-   <!-- Start Header -->
-   <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/assets/html/header.php'; ?>
-   <!-- End Header -->
-   <!-- ======= Our Services Section ======= -->
-
-   <!-- ======= Stats Counter Section ======= -->
-   <section id="comunidad" class="p-2">
-      <div class="container" data-aos="fade-up">
-         <div class="container">
-            <nav aria-label="breadcrumb">
-               <ol class="breadcrumb">
-                  <li class="breadcrumb-item"><a href="index#index">Inicio</a></li>
-                  <li class="breadcrumb-item active" aria-current="page">Beneficios</a></li>
-               </ol>
-            </nav>
-         </div>
-         <div class="row gy-2 align-items-center">
-            <div class="col-md-12 col-sm-12 text-center">
-               <div class="row">
-                  <div class="section-header pb-0">
-                     <h2 class="text-pri">Beneficios y Recompensas</h2>
-                  </div>
+<body class="g-sidenav-show  bg-gray-200">
+   <!-- Start Slider -->
+   <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/system/assets/html/slider_user.php'; ?>
+   <!-- End Slider -->
+   <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
+      <iframe src="<?= $url .'/benefits.php' ?>" frameborder="0" style="width: 100%; height: 100%;" class="vh-100"></iframe>
+   </main>
+   <div id="preloader">
+      <img src="/assets/image/favicon_0.ico" alt="Cargando...">
+   </div>
+   <!-- ======= Basic Modal ======= -->
+   <form method="POST">
+      <div class="modal fade" id="basicModal" tabindex="-1">
+         <div class="modal-dialog">
+            <div class="modal-content">
+               <div class="modal-header">
+                  <h5 class="modal-title">Salir del sistema</h5>
+                  <button type="button" class="btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
                </div>
-               <div>
-                  <h5 class="text-black">Descubre los beneficios exclusivos que tenemos para ti</h5>
+               <div class="modal-body">
+                  ¿Esta seguro que desea salir del sistema?
                </div>
-               <div class="mt-5">
-                  <div class="row text-start">
-                     <div class="col-md-9">
-                        <div class="row">
-                           <?= $benefitPageIndex ?>
-                        </div>
-                     </div>
-                     <div class="col-md-3">
-                        <div class="container my-5">
-                           <div class="row justify-content-center">
-                              <?= $cardDiscount ?>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
+               <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"><i class="material-icons opacity-10">close</i> Cerrar</button>
+                  <button type="submiT" name="logout" class="btn btn-danger"><i class="material-icons opacity-10">logout</i> Salir del sistema</button>
                </div>
             </div>
          </div>
       </div>
-   </section>
-   <!-- Start Footer -->
-   <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/assets/html/footer.php'; ?>
-   <!-- End Footer -->
+   </form>
+   <!--   Core JS Files   -->
+   <script src="/system/assets/js/core/popper.min.js"></script>
+   <script src="/system/assets/js/core/bootstrap.min.js"></script>
+   <script src="/system/assets/js/plugins/perfect-scrollbar.min.js"></script>
+   <script src="/system/assets/js/plugins/smooth-scrollbar.min.js"></script>
+   <script src="/system/assets/js/plugins/chartjs.min.js"></script>
 
-   </main><!-- End #main -->
-   <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-
-   <div id="preloader">
-      <img src="/assets/image/favicon_0.ico" alt="Cargando...">
-   </div>
-
-   <!-- Vendor JS Files -->
-   <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-   <script src="assets/vendor/aos/aos.js"></script>
-   <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-   <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
-   <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-   <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-   <script src="assets/vendor/php-email-form/validate.js"></script>
-
-   <!-- Template Main JS File -->
-   <script src="assets/js/main.js"></script>
-
+   <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
+   <script src="/system/assets/js/material-dashboard.min.js?v=3.1.0"></script>
 </body>
 
 </html>
