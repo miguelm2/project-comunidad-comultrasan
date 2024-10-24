0f2c4de78a34294de319b42a01fa945e0a42c85a
Miguel Martinez <mmartinez@startnet.co>
2024-10-24 14:08:34 -0500

    Update slider_admin.php

--- system/assets/html/slider_admin.php
+++ system/assets/html/slider_admin.php
@@ -33,7 +33,7 @@
                <ul class="dropdown-menu px-2 py-3 w-auto" aria-labelledby="navbarDropdownMenuLink2">
                   <li class="dropdown-item"><a href="questions"> Preguntas frecuentes</a></li>
                   <li class="dropdown-item"><a href="benefitsPage"> Beneficios Pagina</a></li>
-                  <li class="dropdown-item"><a href="typeComunities"> Tipos de comunidad</a></li>
+                  <li class="dropdown-item"><a href="typeComunities"> Grupos de Interes</a></li>
                   <li class="dropdown-item"><a href="discounts"> Descuentos</a></li>
                   <li class="dropdown-item"><a href="eventsCalendar"> Calendario Eventos</a></li>
                </ul>
